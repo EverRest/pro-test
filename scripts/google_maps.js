a871eb980bbff34dd3fbe43d4a786c6578ff0d30
Pavlo Medynskyy <Pavlo Medynskyy>
2016-01-25 15:07:34 +0300

    new style for info window

--- scripts/google_maps.js
+++ scripts/google_maps.js
@@ -60,17 +60,12 @@ var marker = new google.maps.Marker({
 	map: map,
 	});
 
-var partial = "<table>" +
-    "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
-    "<tr><td>Address:</td> <td><input type='text' id='address'/></td> </tr>" +
-    "<tr><td>Type:</td> <td><select id='type'>" +
-    "<option value='dog' SELECTED>dog</option>" +
-    "<option value='cat'>cat</option>" +
-    "<option value='bird'>bird</option>" +
-     "<option value='other'>other</option>" +
-    "</select> </td></tr>" +
-    "<tr><td>Radius</td><td><input type='text' id = 'radius'> </td> </tr>" +
-    "</table>";
+var partial = "<div class='right'>" +
+    "<p>Name: " + "<span> Rex </span>" + " </p>" +
+    "<p>Type: <span> Dog</span></p>" +
+    "<p>Sort: <span> Golden Retriever</span></p>" +
+    "<p>Descrition: <span>45кг з чорним носом, гарна і вихована,весела і в  плямку.</span>" +
+    "</div>" + "<div class='left'><img alt='' src='./img/dog.png' />" +"</div>";
 
 var infowindow = new google.maps.InfoWindow({
 	content: partial
